e099eb4b5af37d7574d0e46640b3f7395658cace
unknown <547806059@qq.com>
2018-05-01 16:19:13 +0800

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/resumer/dist',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
